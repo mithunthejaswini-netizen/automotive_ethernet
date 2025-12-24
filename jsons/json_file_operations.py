d923bbf797800f684638bc6e3c0c6b6eb64ef11c
MithunThejaswini <mithunthejaswini@gmail.com>
2025-12-24 17:42:43 +0530

    [Mithun.v] comming json changes

--- jsons/json_file_operations.py
+++ jsons/json_file_operations.py
@@ -1,6 +1,7 @@
 import json
 from pathlib import Path
 
+# reading the complete content of json file
 def parse_json_file_and_read_data(json_path):
     print(json_path)
     if Path(json_path).exists():
